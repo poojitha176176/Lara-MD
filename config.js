ea8b4fdb8cd3f145e4c12a0aebe63da4203a812a
poojitha176176 <poojithaakash0@gmail.com>
2024-12-28 17:32:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LiIViZDB#1iWpkNuKSolGHWAQmI6LZCbZ7jqqEzWw_BaIG3GMgdg", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "Li5wHBpZ#Sq0auRxtx4SY95lpCEtUly1nRrmQQ34LLqLmM7faibo", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",   
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
